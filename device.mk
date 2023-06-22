83f76d30594939e9970dbf4659dbc36c6ea85b27
dogpoopy <qtqtron@gmail.com>
2023-06-22 23:35:23 +0800

    vayu: add component for removing prebuilt packages

--- device.mk
+++ device.mk
@@ -398,6 +398,10 @@ PRODUCT_COPY_FILES += \
 # Properties
 include $(LOCAL_PATH)/properties/default.mk
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.6.vendor \
