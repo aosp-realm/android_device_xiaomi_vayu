7973e6d795464a669934e3322c533f983cf6abf2
dogpoopy <qtqtron@gmail.com>
2023-11-04 14:31:39 +0800

    vayu: Adapt to PE

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -12,7 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common AOSP configurations
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Feature Flags
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_MIUI_CAMERA := true
 
 PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
