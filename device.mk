c0e1c8f9048e54d1cc401b5929ca363106b16ee1
MADMAX <madmax78968@gmail.com>
2024-01-01 01:39:39 +0800

    vayu: Import Dolby audio blobs

--- device.mk
+++ device.mk
@@ -346,7 +346,10 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libavservices_minijail_vendor \
+    libcodec2_soft_common.vendor \
+    libsfplugin_ccodec_utils.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
