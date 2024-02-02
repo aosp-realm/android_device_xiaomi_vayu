9c6307134944f030c80f873bab9d037064c44b04
dogpoopy <qtqtron@gmail.com>
2024-02-02 13:40:12 +0800

    vayu: Switch miuicamera repo

--- device.mk
+++ device.mk
@@ -114,7 +114,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
    libstdc++.vendor
 
-$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/vayu-miuicamera/products/miuicamera.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
