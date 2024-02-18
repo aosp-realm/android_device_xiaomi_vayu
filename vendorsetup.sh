83d27b2a22beb2235ba27764a412e9da9dcb674e
dogpoopy <dogpoopy.dev@proton.me>
2024-02-19 07:41:43 +0800

    vayu: Always remove duplicate genfs entries
    * Needs to retain dt rules
    
    Signed-off-by: dogpoopy <dogpoopy.dev@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,5 @@ if [ -d "prebuilts/vndk/v33" ]; then
 fi
 
 git clone https://android.googlesource.com/platform/prebuilts/vndk/v33 --depth=1 "prebuilts/vndk/v33"
+
+find device/qcom/sepolicy_vndr -type f -exec sed -i '/\/devices\/platform\/soc\/a84000\.i2c\/i2c-2\/2-0028\/wakeup/d' {} +
