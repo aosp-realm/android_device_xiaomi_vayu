3dff4029dd55a6700d6ee04bc0339e575513620b
dogpoopy <dogpoopy.dev@proton.me>
2024-03-12 01:27:49 +0800

    Revert "vayu: Always clone/reclone VNDK v33"
    This reverts commit 99fa04924fcf04f811ccc80e5a40235eaf3340d6.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,1 @@
-if [ -d "prebuilts/vndk/v33" ]; then
-    rm -rf "prebuilts/vndk/v33"
-fi
-
-git clone https://android.googlesource.com/platform/prebuilts/vndk/v33 --depth=1 "prebuilts/vndk/v33"
-
 find device/qcom/sepolicy_vndr -type f -exec sed -i '/\/devices\/platform\/soc\/a84000\.i2c\/i2c-2\/2-0028\/wakeup/d' {} +
