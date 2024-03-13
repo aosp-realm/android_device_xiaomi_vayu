118a7f5381464dfae6f4f1846d768e032c95a74e
dogpoopy <dogpoopy.dev@proton.me>
2024-03-13 18:38:31 +0800

    vayu: Drop specified clang for kernel compilation
    This commit reverts e3605455e65197a7697fd68f708913458bbbda06.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,8 +88,6 @@ BOARD_KERNEL_IMAGE_NAME := Image
 ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CONFIG := vayu_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
-  TARGET_KERNEL_CLANG_VERSION := r498229b
-  TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
   TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
   TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 endif
