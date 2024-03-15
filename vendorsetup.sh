285f0da2522a7946bd96d7671478350b93a74d1e
dogpoopy <dogpoopy.dev@proton.me>
2024-03-15 16:01:48 +0800

    vayu: Always clone/reclone gms
    * Repo won't sync files/apk with lfs tag ¯\_(ツ)_/¯

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,7 @@
 find device/qcom/sepolicy_vndr -type f -exec sed -i '/\/devices\/platform\/soc\/a84000\.i2c\/i2c-2\/2-0028\/wakeup/d' {} +
+
+if [ -d "vendor/gms" ]; then
+    rm -rf "vendor/gms"
+fi
+
+git clone https://gitlab.com/PixelOS-Devices/vendor_gms.git -b fourteen --depth=1 vendor/gms
